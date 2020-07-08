381c1dc736b2b9a4eb5c776a07dbd5525b4312de
Ilhee <asahi8769@gmail.com>
2020-07-09 00:20:00 +0900

    Apply all changes

--- git_.py
+++ git_.py
@@ -5,12 +5,15 @@ import os
 class GitCommandLines():
     def __init__(self):
         self.repository = r'https://github.com/asahi8769/reinforcement-learning.git'
+        self.rel_dir = None
         subprocess_cmd (f'git config --global user.name Ilhee Lee')
         subprocess_cmd (f'git config --global user.email asahi8769@gmail.com')
 
     def push_rep(self):
         self.clone_rep()
-        self.ask_overwrite()
+        if self.ask_overwrite() is False:
+            os.startfile(self.rel_dir)
+            return
         subprocess_cmd (f'git init')
         subprocess_cmd (f'git add .')
         subprocess_cmd (f'git config --global http.sslVerify false')
@@ -20,15 +23,15 @@ class GitCommandLines():
         subprocess_cmd (f'git remote remove origin')
 
     def clone_rep(self):
-        rel_dir = os.path.relpath(old_ver_directory(), os.getcwd())
-        subprocess_cmd (f'git clone {self.repository[:-4]} {rel_dir}')
+        self.rel_dir = os.path.relpath(old_ver_directory(), os.getcwd())
+        subprocess_cmd (f'git clone {self.repository[:-4]} {self.rel_dir}')
 
     def history(self):
         subprocess_cmd (f'git log ')
 
     def ask_overwrite(self):
-        if input('Overwrite current repository? (y/n, Default : n)').lower() != 'y':
-            return
+        if input('Overwrite current repository? (y/n, Default : n) :').lower() != 'y':
+            return False
 
 
 if __name__ == "__main__":
